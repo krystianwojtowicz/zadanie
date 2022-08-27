9c5a871c679881f69912b07f4412c5e3ae4044e6
Krystian <krystian1wojtowicz@gmail.com>
2022-08-27 14:15:18 +0200

    vefore form

---
+++ main.js
@@ -0,0 +1,6 @@
+const toggleButton = document.querySelector(".navbar-toggle");
+const navbarLinks = document.querySelectorAll(".navbar-links");
+toggleButton.addEventListener("click", function () {
+  for (let i = 0; i < navbarLinks.length; i++)
+    navbarLinks[i].classList.toggle("active");
+});
