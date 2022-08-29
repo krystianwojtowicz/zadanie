c0b36ecb03afe6bd29efa3aeeb046ddb1ef560bc
Krystian <krystian1wojtowicz@gmail.com>
2022-08-29 12:50:01 +0200

    before changing checkbox

--- main.js
+++ main.js
@@ -20,9 +20,9 @@ toggleButton.addEventListener("click", function () {
 
 const form = document.getElementById("form");
 const locations = document.getElementById("locations");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const password2 = document.getElementById("password2");
+const checkin = document.getElementById("checkin");
+const checkout = document.getElementById("checkout");
+const guest = document.getElementById("guest");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -55,17 +55,17 @@ function checking() {
   if (counter === 4) console.log("przesłanie formularza");
 }
 
-const isValidEmail = (email) => {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-};
+// const isValidEmail = (email) => {
+//   const re =
+//     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//   return re.test(String(email).toLowerCase());
+// };
 
 const validateInputs = () => {
   const locationValue = locations.value.trim();
-  const emailValue = email.value.trim();
-  const passwordValue = password.value.trim();
-  const password2Value = password2.value.trim();
+  const checkinValue = checkin.value.trim();
+  const checkoutValue = checkout.value.trim();
+  const guestValue = guest.value.trim();
 
   if (locationValue === "") {
     setError(locations, "Location is required");
@@ -73,27 +73,21 @@ const validateInputs = () => {
     setSuccess(locations);
   }
 
-  if (emailValue === "") {
-    setError(email, "Email is required");
-  } else if (!isValidEmail(emailValue)) {
-    setError(email, "Provide a valid email address");
+  if (checkinValue === "") {
+    setError(checkin, "Check in is required");
   } else {
-    setSuccess(email);
+    setSuccess(checkin);
   }
 
-  if (passwordValue === "") {
-    setError(password, "Password is required");
-  } else if (passwordValue.length < 8) {
-    setError(password, "Password must be at least 8 character.");
+  if (checkoutValue === "") {
+    setError(checkout, "Check out is required");
   } else {
-    setSuccess(password);
+    setSuccess(checkout);
   }
 
-  if (password2Value === "") {
-    setError(password2, "Please confirm your password");
-  } else if (password2Value !== passwordValue) {
-    setError(password2, "Passwords doesn't match");
+  if (guestValue === "") {
+    setError(guest, "Guest is required");
   } else {
-    setSuccess(password2);
+    setSuccess(guest);
   }
 };
